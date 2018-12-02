6367d990208578dd08d0b9dbf9df1a853ce69f8c
Kasey Carni <carni.k@knights.ucf.edu>
2018-12-01 19:44:02 -0500

    final build of game

---
+++ Climber copy/EscapeTheTower/Assets/Scripts/KACA_Camera_Controller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KACA_Camera_Controller : MonoBehaviour {
+
+    public GameObject player;       
+
+    private Vector3 offset;        
+    void Start()
+    {
+
+        offset = transform.position - player.transform.position;
+
+    }
+
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
+
---
+++ Climber copy/EscapeTheTower/Assets/Scripts/KACA_Hazard.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KACA_Hazard : MonoBehaviour
+{
+
+    public Transform target;
+
+    public int moveSpeed;
+
+    public int rotationSpeed;
+
+    private Transform myTransform;
+
+
+    void Awake()
+    {
+        myTransform = transform;
+    }
+
+    void Start()
+    {
+        GameObject go = GameObject.FindGameObjectWithTag("Player");
+
+        target = go.transform;
+    }
+    
+
+    void Update()
+    {
+        Vector3 dir = target.position - myTransform.position;
+        dir.z = 0.0f;
+        if (dir != Vector3.zero)
+        {
+            myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.FromToRotation(Vector3.right, dir), rotationSpeed * Time.deltaTime);
+        }
+
+    }
+
+}
